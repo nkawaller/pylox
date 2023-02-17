c7862521d362d68f7e36d5205785dd8c6f4f2c20
nkawaller <kawaller@gmail.com>
2023-02-17 13:12:35 -0500

    refactor class

--- Lox.py
+++ Lox.py
@@ -1,21 +1,28 @@
 import os
+import sys
 
 class Lox:
 
+    args = sys.argv[1:]
+
     @classmethod
-    def main(cls, *args):
-        if len(args) > 1:
-            print("Usage: pylox [script]")
-        if len(args) == 1:
-            cls.runfile(args[0])
+    def main(cls):
+        if len(cls.args) > 1:
+            print("Usage: python3 Lox.py [script]")
+            sys.exit(1)
+        elif len(cls.args) == 1:
+            cls.run_file(cls.args[0])
         else:
             cls.run_prompt()
 
-
     @classmethod
     def run_file(cls, file):
-        os.system(f"python3 {file}")
+        with open(file, "r") as reader:
+            print(reader.read())
 
     @classmethod
     def run_prompt(cls):
-        # Run prompt
+        print("running prompt")
+
+if __name__ == '__main__':
+    Lox.main()
