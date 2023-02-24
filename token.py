850481e8b8e1b37d9f25f4ce9164c26a6bc2e5e2
nkawaller <kawaller@gmail.com>
2023-02-24 06:30:01 -0500

    refactor token and tokentypes

---
+++ token.py
@@ -0,0 +1,12 @@
+"""Token class that contains line numbers to report errors."""
+
+class Token:
+
+    def __init__(self, type, lexeme, literal, line):
+        self.type = type
+        self.lexeme = lexeme
+        self.literal = literal
+        self.line = line
+
+    def __str__(self):
+        return f"{self.type} {self.lexeme} {self.line}"
---
+++ tokentypes.py
@@ -0,0 +1,55 @@
+"""Class of types to identify lexemes"""
+
+from enum import Enum
+
+
+class TokenType(Enum):
+    """Names of token identifiers"""
+
+    # Single-character tokens.
+    LEFT_PAREN = "("
+    RIGHT_PAREN = ")"
+    LEFT_BRACE = "{"
+    RIGHT_BRACE = "}"
+    COMMA = ","
+    DOT = "."
+    MINUS = "-"
+    PLUS = "+"
+    SEMICOLON = ";"
+    SLASH = "/"
+    STAR = "*"
+
+    # One or two character tokens.
+    BANG = "!"
+    BANG_EQUAL = "!="
+    EQUAL = "="
+    EQUAL_EQUAL = "=="
+    GREATER = ">"
+    GREATER_EQUAL = ">="
+    LESS = "<"
+    LESS_EQUAL = "<="
+
+    # Literals
+    IDENTIFIER = "IDENTIFIER"
+    STRING = "STRING"
+    NUMBER = "NUMBER"
+
+    # Keywords
+    AND = "AND"
+    CLASS = "CLASS"
+    ELSE = "ELSE"
+    FALSE = "FALSE"
+    FUN = "FUN"
+    FOR = "FOR"
+    IF = "IF"
+    NIL = "NIL"
+    OR = "OR"
+    PRINT = "PRINT"
+    RETURN = "RETURN"
+    SUPER = "SUPER"
+    THIS = "THIS"
+    TRUE = "TRUE"
+    VAR = "VAR"
+    WHILE = "WHILE"
+
+    EOF = "EOF"
