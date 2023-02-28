305302057de706638091712b0e088d29f69c7e34
nkawaller <kawaller@gmail.com>
2023-02-28 06:47:39 -0500

    continue scanner

--- scanner.py
+++ scanner.py
@@ -1,5 +1,6 @@
 """Scanner class"""
 
+from lox import Lox
 from tokentypes import TokenType
 from token import Token
 
@@ -24,7 +25,7 @@ class Scanner:
         self.tokens.append(Token(TokenType.EOF, "", None, self.line))
         return tokens
 
-    def scan_token():
+    def scan_token(self):
         # TODO: May need this in a while loop, need an else at the end
         c = advance()
         if c == '(':
@@ -57,11 +58,50 @@ class Scanner:
         elif c == '*':
             add_token(STAR)
             break
+        elif c == '!':
+            add_token(BANG_EQUAL if self.match('=') else BANG)
+            break
+        elif c == '=':
+            add_token(EQUAL_EQUAL if self.match('=') else EQUAL)
+            break
+        elif c == '<':
+            add_token(LESS_EQUAL if self.match('=') else LESS)
+            break
+        elif c == '>':
+            add_token(GREATER_EQUAL if self.match('=') else GREATER)
+            break
+        elif c == '/':
+            if match('/'):
+                while peek() is not '\n' and not self.is_at_end:
+                    advance()
+            else:
+                self.add_token(SLASH)
+            break
+        else:
+            Lox.error(self.line, "Unexpected character.")
+            break
+
+    def match(self, expected):
+        """Like a conditional advance()"""
+        if self.is_at_end():
+            return False
+        if self.source.charAt(self.current) is not expected:
+            return False
+        self.current += 1
+        return True
+
+    def peek(self):
+        """Lookahead method"""
+        if self.is_at_end():
+            return '\0'
+        return self.source.charAt(self.current)
 
     def is_at_end(self):
         return self.current >= len(self.source)
 
     def advance(self):
+        # charAt looks at a string, and gives you the char for the
+        # index you provide
         return self.source.charAt(self.current += 1)
 
     def add_token(self, type):
