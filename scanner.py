e55f08917569e6f53a933997150d9f6d5053b7f2
nkawaller <kawaller@gmail.com>
2023-02-27 19:31:08 -0500

    start scan_token method

--- scanner.py
+++ scanner.py
@@ -24,5 +24,49 @@ class Scanner:
         self.tokens.append(Token(TokenType.EOF, "", None, self.line))
         return tokens
 
-    def is_at_end():
-        return current >= len(source)
+    def scan_token():
+        # TODO: May need this in a while loop, need an else at the end
+        c = advance()
+        if c == '(':
+            add_token(LEFT_PAREN)
+            break
+        elif c == ')':
+            add_token(RIGHT_PAREN)
+            break
+        elif c == '{':
+            add_token(LEFT_BRACE)
+            break
+        elif c == '}':
+            add_token(RIGHT_BRACE)
+            break
+        elif c == ',':
+            add_token(COMMA)
+            break
+        elif c == '.':
+            add_token(DOT)
+            break
+        elif c == '-':
+            add_token(MINUS)
+            break
+        elif c == '+':
+            add_token(PLUS)
+            break
+        elif c == ';':
+            add_token(SEMICOLON)
+            break
+        elif c == '*':
+            add_token(STAR)
+            break
+
+    def is_at_end(self):
+        return self.current >= len(self.source)
+
+    def advance(self):
+        return self.source.charAt(self.current += 1)
+
+    def add_token(self, type):
+        add_token(type, None)
+
+    def add_token(self, type, literal):
+        text = self.source.substring(self.start, self.current)
+        self.tokens.add(Token(type, text, literal, self.line))
