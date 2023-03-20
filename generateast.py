f1b8c91f2aeafb370186ae02416f9bdd0a7714dc
nkawaller <kawaller@gmail.com>
2023-03-20 19:29:54 -0400

    add define_ast and define_type functions

--- generateast.py
+++ generateast.py
@@ -1,9 +1,25 @@
 """Script that generates expr.py"""
 
-def write_file():
-    """Write the code"""
+import sys
 
-    with open("expr.py", "w", encoding='utf-8') as ast:
+
+def define_type(lines, basename, classname, fields):
+    """Write each expression class"""
+
+    lines.append(f"\tclass {classname}({basename}):\n\n"
+                 f"\t\tdef __init__(self, {fields}):\n")
+    fields = fields.split(", ")
+    for field in fields:
+        name = field.split(" ")[0]
+        lines.append(f"\t\t\tself.{name} = {name}\n")
+    lines.append("\n")
+
+
+def define_ast(output_dir, basename, types):
+    """Generate code for syntax tree"""
+
+    path = f"{output_dir}{basename.lower()}.py"
+    with open(path, "w", encoding='utf-8') as ast:
         lines = [
             "\"\"\"Syntax tree that represents the Lox grammar\"\"\"",
             "\n",
@@ -11,21 +27,33 @@ def write_file():
             "from abc import ABC",
             "\n",
             "\n",
-            "class Expr(ABC):\n",
-            "\t\"\"\"Expression base class\"\"\""
-            "\n",
-            "\n",
-            "\tdef __init__(self, left, operator, right):\n",
-            "\t\tself.left = left\n",
-            "\t\tself.operator = operator\n",
-            "\t\tself.right = right\n"
+            f"class {basename}(ABC):\n",
+            "\n"
         ]
+        for expr_type in types:
+            classname = expr_type.split(":")[0].strip()
+            fields = expr_type.split(":")[1].strip()
+            define_type(lines, basename, classname, fields)
         ast.writelines(lines)
+
 
 def main():
     """Script entry point"""
+    args = sys.argv[1:]
+    if len(args) != 1:
+        print("Usage: python3 generateast.py [output directory]")
+        sys.exit(1)
+    else:
+        output_dir = args[0]
+        expr_list = [
+            # Could this be a dict?
+            "Binary   : left, operator, right",
+            "Grouping : expression",
+            "Literal  : value",
+            "Unary    : operator, right"
+        ]
+        define_ast(output_dir, "Expr", expr_list)
 
-    write_file()
 
 if __name__ == "__main__":
     main()
