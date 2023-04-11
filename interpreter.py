74f74a08041ac543310be2215106afe9dd537c16
nkawaller <kawaller@gmail.com>
2023-04-11 18:47:06 -0400

    continue interpreter

--- interpreter.py
+++ interpreter.py
@@ -1,4 +1,19 @@
+"""Interpreter class"""
+
 import expr
 
 class Interpreter(expr.Visitor):
-    pass
+    """Using the visitor pattern, execute the syntax tree itself"""
+    
+    def visit_literal_expr(e):
+        """Evaluate literal expressions"""
+
+        return e.value
+
+    def visit_grouping_expr(self, e):
+        """Evaluate grouping expressions"""
+
+        return self.evaluate(e.expression)
+
+    def evaluate(self, e):
+        return e.accept(self)
