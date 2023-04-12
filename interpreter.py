09601973bb479f2a28a722cc5f3fb344b8e8e310
nkawaller <kawaller@gmail.com>
2023-04-12 19:00:37 -0400

    convert unary if else to map

--- interpreter.py
+++ interpreter.py
@@ -15,5 +15,37 @@ class Interpreter(expr.Visitor):
 
         return self.evaluate(e.expression)
 
+    def visit_unary_expr(self, e):
+        """Evaluate unary expressions"""
+
+        right = self.evaluate(e.right)
+
+        # if e.operator.tokentype == "BANG":
+        #     return not self.is_truthy(right)
+        # if e.operator.tokentype == "MINUS":
+        #     return -float(right)
+
+        unary_map = {
+            "BANG": lambda right: not self.is_truthy(right),
+            "MINUS": lambda right: -float(right)
+        }
+
+        return unary_map.get(e.operator.tokentype, None)(right)
+
+    def is_truthy(self, object):
+        """Evaluate an object's truthiness. False and Nil are false,
+        everything else is true
+        """
+
+        if object == None:
+            return False
+        if isinstance(object, bool):
+            return bool(object)
+        return True
+
     def evaluate(self, e):
+        """Send the expression back into the interpreter's visitor
+        implementation
+        """
+
         return e.accept(self)
