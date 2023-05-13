f98933606ee87226fdf6663dfd11fa7e4c8113d1
nkawaller <kawaller@gmail.com>
2023-05-12 20:48:28 -0400

    continue resolver

--- resolver.py
+++ resolver.py
@@ -8,6 +8,8 @@ class Resolver(expr.Visitor, stmt.Visitor):
     """Implements expr and stmt visitors because we visit every node
     in the syntax tree
     """
+    
+    scopes = []
 
     def __init__(self, interpreter):
         self.interpreter = interpreter
@@ -16,6 +18,14 @@ class Resolver(expr.Visitor, stmt.Visitor):
         self.begin_scope()
         self.resolve(s.statements)
         self.endScope()
+        return None
+
+    def visit_var_stmt(self, s):
+        self.declare(s.name)
+        # TODO: shorten this
+        if s.initializer is not None:
+            self.resolve(s.initializer)
+        self.define(s.name)
         return None
 
     def resolve(self, statements):
@@ -27,3 +37,21 @@ class Resolver(expr.Visitor, stmt.Visitor):
 
     def resolve_expr(self, e):
         e.accept(self)
+
+    def begin_scope(self):
+        self.scopes.append({})
+
+    def end_scope(self):
+        self.scopes.pop()
+
+    def declare(self, name):
+        if not self.scopes: return
+        # Using last index instead of peek()
+        scope = self.scopes[-1]
+        scope[name.lexeme] = False
+
+    def define(self, name):
+        if not self.scopes: return
+        # Using last index instead of peek()
+        self.scopes[-1][name.lexeme] = True
+    
